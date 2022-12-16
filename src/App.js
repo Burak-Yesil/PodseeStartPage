a5156069b3453cb1b46b54d7b7880b5b2ca5644f
Burak <byesil@stevens.edu>
2022-12-15 21:25:57 -0500

    updated files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './PodseeLogo3.svg';
 import './App.css';
 
 function App() {
@@ -6,17 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h2>
+          See the Discussion
+        </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Updates Coming Soon!!!
+        </p>       
       </header>
     </div>
   );
